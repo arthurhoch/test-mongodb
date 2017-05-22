b55cfdfd197e03d969b40171dc1cc7239e72f565
Arthur Hoch <arthur.j.h96@gmail.com>
2017-05-22 14:32:45 -0300

    Test DELETE

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect')
 const request = require('supertest')
+const { ObjectId } = require('mongodb')
 
 const { app } = require('./../server')
 const { Todo } = require('./../models/todo')
 
 const todos = [{
+	_id: new ObjectId(),
 	text: 'First test todo'
 }, {
+	_id: new ObjectId(),
 	text: 'Second test todo'
 }]
 
@@ -67,6 +70,75 @@ describe('GET /todos', () => {
 			.expect((res) => {
 				expect(res.body.todos.length).toBe(2)
 			})
+			.end(done)
+	})
+})
+
+describe('GET /todos/:id', () => {
+	it('should return todo doc', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text)
+			})
+			.end(done)
+	})
+
+	it('should return 404 if todo not found', (done) => {
+		var hexId = new ObjectId().toHexString();
+
+		request(app)
+			.get(`/todos/${hexId}`)
+			.expect(404)
+			.end(done)
+	})
+
+	it('sould return 404 for non-object ids', (done) => {
+		request(app)
+			.get('/todos/123')
+			.expect(404)
+			.end(done)
+	})
+
+})
+
+describe('DELETE /todos/:id', () => {	
+
+	it('should remove a todo', (done) => {
+		var hexId = todos[1]._id.toHexString()
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(hexId)
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err)
+				}
+
+				Todo.findById(hexId).then((todo) => {
+					expect(todo).toNotExist()
+					done()
+				}).catch((e) => done(e))
+
+			})
+	})
+
+	it('should return 404 if todo not found', (done) => {
+		var hexId = new ObjectId()
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(404)
+			.end(done)
+	})
+
+	it('should return 404 id a object id is invalid', (done) => {
+		request(app)
+			.delete('/todos/123')
+			.expect(404)
 			.end(done)
 	})
 })
